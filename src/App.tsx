8a28085db1ae8dbad12501d254bc5d79092edc17
Antonio MM <antoniomm80@gmail.com>
2025-01-23 23:40:00 +0100

    feat: add shadcn sidebar and related components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 import {QueryClient, QueryClientProvider} from "react-query";
 import Vocabulary from "@/vocabulary/Vocabulary.tsx";
+import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar.tsx";
+import {AppSidebar} from "@/components/app-sidebar.tsx";
+import {Separator} from "@/components/ui/separator.tsx";
+import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator} from "@/components/ui/breadcrumb.tsx";
 
 const queryClient = new QueryClient()
 
@@ -8,7 +12,34 @@ function App() {
     return (
         <>
             <QueryClientProvider client={queryClient}>
-                <Vocabulary/>
+
+                <SidebarProvider>
+                    <AppSidebar/>
+                    <SidebarInset>
+                        <header
+                            className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                            <div className="flex items-center gap-2 px-4">
+                                <SidebarTrigger className="-ml-1"/>
+                                <Separator orientation="vertical" className="mr-2 h-4"/>
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbItem className="hidden md:block">
+                                            <BreadcrumbLink href="#">
+                                                Daily English
+                                            </BreadcrumbLink>
+                                        </BreadcrumbItem>
+                                        <BreadcrumbSeparator className="hidden md:block"/>
+                                        <BreadcrumbItem>
+                                            <BreadcrumbPage>Vocabulary Refresher</BreadcrumbPage>
+                                        </BreadcrumbItem>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                            </div>
+                        </header>
+                        <Vocabulary/>
+                    </SidebarInset>
+                </SidebarProvider>
+
             </QueryClientProvider>
         </>
     )
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,116 @@
+import * as React from "react"
+import {BookOpen, Bot, Settings2, SquareTerminal,} from "lucide-react"
+
+import {NavMain} from "@/components/nav-main"
+import {Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail,} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+
+    navMain: [
+        {
+            title: "Grammar - B2",
+            url: "#",
+            icon: SquareTerminal,
+            isActive: true,
+            items: [
+                {
+                    title: "History",
+                    url: "#",
+                },
+                {
+                    title: "Starred",
+                    url: "#",
+                },
+                {
+                    title: "Settings",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Grammar - C1",
+            url: "#",
+            icon: Bot,
+            items: [
+                {
+                    title: "Genesis",
+                    url: "#",
+                },
+                {
+                    title: "Explorer",
+                    url: "#",
+                },
+                {
+                    title: "Quantum",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Vocabulary",
+            url: "#",
+            icon: BookOpen,
+            items: [
+                {
+                    title: "Daily Random Item",
+                    url: "#",
+                },
+                {
+                    title: "Get Started",
+                    url: "#",
+                },
+                {
+                    title: "Tutorials",
+                    url: "#",
+                },
+                {
+                    title: "Changelog",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Settings",
+            url: "#",
+            icon: Settings2,
+            items: [
+                {
+                    title: "General",
+                    url: "#",
+                },
+                {
+                    title: "Team",
+                    url: "#",
+                },
+                {
+                    title: "Billing",
+                    url: "#",
+                },
+                {
+                    title: "Limits",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+
+}
+
+export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain}/>
+
+            </SidebarContent>
+            <SidebarFooter>
+
+            </SidebarFooter>
+            <SidebarRail/>
+        </Sidebar>
+    )
+}
