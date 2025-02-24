bc8fc21a6b7e71cd68a6445af08b3c18dc6f9599
Antonio MM <antoniomm80@gmail.com>
2025-02-24 19:50:47 +0100

    feat: Vocabulary section revamp
    Skeleton instead of loading spinner
    Stylistic changes

--- src/vocabulary/Vocabulary.tsx
+++ src/vocabulary/Vocabulary.tsx
@@ -2,9 +2,10 @@ import {useQuery} from "react-query";
 import vocabularyService from "@/vocabulary/VocabularyService.ts";
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {Spinner} from "@/components/ui/spinner.tsx";
 import {useBreadcrumb} from "@/breadcrumb/BreadcrumbContext.tsx";
 import {useEffect} from "react";
+import {Skeleton} from "@/components/ui/skeleton.tsx";
+import {Separator} from "@radix-ui/react-separator";
 
 
 const blacklist: string[] = [];
@@ -20,8 +21,9 @@ function Vocabulary() {
         isLoading,
         data,
         refetch,
-        isFetching
+        isFetching,
     } = useQuery(["daily-vocabulary"], () => vocabularyService.getDailyVocabulary(blacklist));
+    const isWaitingForBackend = isLoading || isFetching;
 
     function printArray(array: string[] | undefined) {
         if (!array) {
@@ -31,38 +33,47 @@ function Vocabulary() {
     }
 
 
-    if (isLoading || isFetching) {
-        return (
-            <div className="path-list">
-                <Spinner>
-                    Loading...
-                </Spinner>
-            </div>
-        );
-    }
     if (data?.word && !blacklist.includes(data.word)) {
         blacklist.push(data.word);
     }
     return <div className="p-4">
-        <Card className="bg-zinc-100">
+        <Card>
             <CardHeader>
                 <CardTitle className="text-2xl font-bold">Here's your daily random vocabulary refresher</CardTitle>
-                <CardDescription className="text-lg font-gray-900"><strong className="font-bold">From: </strong><span
-                    className="text-gray-500">{data?.source}</span></CardDescription>
+                <Separator className="bg-border h-[1px] w-full my-4"/>
+                <CardDescription className="text-neutral-600">
+                    {isWaitingForBackend &&
+                        <Skeleton className="h-4 w-full mt-3"/>
+                    }
+                    {!isWaitingForBackend &&
+                        <>
+                            <strong className="font-bold">From: </strong>
+                            <span
+                                className="text-gray-500">{data?.source}</span>
+                        </>
+                    }
+                </CardDescription>
             </CardHeader>
             <CardContent>
-                <ul className="list-none text-left">
-                    <li><strong className="font-extrabold">Word: </strong>{data?.word}</li>
-                    <li><strong className="font-extrabold">Definition: </strong>{data?.definition}</li>
-                    <li><strong className="font-extrabold">Part of Speech: </strong>{data?.partOfSpeech}</li>
-                    <li><strong className="font-extrabold">Pronunciation: </strong>{data?.pronunciation}</li>
-                    <li><strong className="font-extrabold">Example Sentence: </strong>{data?.exampleSentence}</li>
-                    <li><strong className="font-extrabold">Collocations: </strong>{printArray(data?.collocations)}</li>
-                    <li><strong className="font-extrabold">Synonyms: </strong>{printArray(data?.synonyms)}</li>
-                </ul>
+
+                {isWaitingForBackend &&
+                    <div className="space-y-2">
+                        <Skeleton className="h-96 w-full"/>
+
+                    </div>}
+                {!isWaitingForBackend &&
+                    <ul className="list-none text-left">
+                        <li><strong className="font-extrabold">Word: </strong>{data?.word}</li>
+                        <li><strong className="font-extrabold">Definition: </strong>{data?.definition}</li>
+                        <li><strong className="font-extrabold">Part of Speech: </strong>{data?.partOfSpeech}</li>
+                        <li><strong className="font-extrabold">Pronunciation: </strong>{data?.pronunciation}</li>
+                        <li><strong className="font-extrabold">Example Sentence: </strong>{data?.exampleSentence}</li>
+                        <li><strong className="font-extrabold">Collocations: </strong>{printArray(data?.collocations)}</li>
+                        <li><strong className="font-extrabold">Synonyms: </strong>{printArray(data?.synonyms)}</li>
+                    </ul>}
             </CardContent>
             <CardFooter className="place-content-end">
-                <Button onClick={() => refetch()}>Refresh</Button>
+                <Button onClick={() => refetch()} disabled={isWaitingForBackend}>Refresh</Button>
             </CardFooter>
         </Card>
     </div>
